4dc43a48402be95898298db92da19d5353237aa3
energymaster22 <energymastervr@gmail.com>
2025-12-20 22:47:44 -0500

    Removed commented out apquest code

--- YARG/options.py
+++ YARG/options.py
@@ -1,18 +1,1 @@
-#from dataclasses import dataclass
-
-#from Options import Choice, OptionGroup, PerGameCommonOptions, Range, Toggle, StartInventoryPool
-
-# In this file, we define the options the player can pick.
-# The most common types of options are Toggle, Range and Choice.
-
-# Options will be in the game's template yaml.
-# They will be represented by checkboxes, sliders etc. on the game's options page on the website.
-# (Note: Options can also be made invisible from either of these places by overriding Option.visibility.
-#  APQuest doesn't have an example of this, but this can be used for secret / hidden / advanced options.)
-
-# For further reading on options, you can also read the Options API Document:
-# https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/options%20api.md
-
-#@dataclass
-#class YARGOptions(PerGameCommonOptions):
-#    start_inventory_from_pool: StartInventoryPool
+#Will be adding options in later version
--- YARG/rules.py
+++ YARG/rules.py
@@ -12,27 +12,9 @@ if TYPE_CHECKING:
 
 
 def set_all_rules(world: YARGWorld) -> None:
-    # In order for AP to generate an item layout that is actually possible for the player to complete,
-    # we need to define rules for our Entrances and Locations.
-    # Note: Regions do not have rules, the Entrances connecting them do!
-    # We'll do entrances first, then locations, and then finally we set our victory condition.
-
     set_all_location_rules(world)
-    #set_completion_condition(world)
 
 def set_all_location_rules(world: YARGWorld) -> None:
-    # Location rules work no differently from Entrance rules.
-    # Most of our locations are chests that can simply be opened by walking up to them.
-    # Thus, their logical requirements are covered by the Entrance rules of the Entrances that were required to
-    # reach the region that the chest sits in.
-    # However, our two enemies work differently.
-    # Entering the room with the enemy is not enough, you also need to have enough combat items to be able to defeat it.
-    # So, we need to set requirements on the Locations themselves.
-    # Since combat is a bit more complicated, we'll use this chance to cover some advanced access rule concepts.
-
-    # Sometimes, you may want to have different rules depending on the player's chosen options.
-    # There is a wrong way to do this, and a right way to do this. Let's do the wrong way first.
-    
     for name in Songs.keys():
         location1 = world.get_location("\"" + str(name) + "\" Item 1")
         location2 = world.get_location("\"" + str(name) + "\" Item 2")
@@ -40,8 +22,3 @@ def set_all_location_rules(world: YARGWorld) -> None:
         set_rule(location1, lambda state, x=item: state.has(x, world.player))
         
         set_rule(location2, lambda state, x=item: state.has(x, world.player))
-
-
-#def set_completion_condition(world: YARGWorld) -> None:
-    
-    
