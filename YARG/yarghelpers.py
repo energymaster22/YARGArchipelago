a9bd8d3aaaf281d643f683f319a7c7aa185fffe1
energymaster22 <energymastervr@gmail.com>
2026-09-06 20:54:33 -0400

    Added Go Down in History setlist and enabled longnames variable

--- YARG/yarghelpers.py
+++ YARG/yarghelpers.py
@@ -41,7 +41,7 @@ def instnamechange(input):
 
 def itemnamefromindex(index):
     from .songinfo import Songs
-    longnames = False
+    longnames = True
 
     if longnames == False:
         return f'"{(Songs.get(index)).songname}" by {(Songs.get(index)).artistname}'
